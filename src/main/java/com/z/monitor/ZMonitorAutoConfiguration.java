f2733e744171eca8b0a44fa6260c5afe702a0507
AungPyaePhyo <aungpyaephyo3112002@gmail.com>
2026-02-23 19:24:04 +0630

    1.0.9 fix bugs

--- src/main/java/com/z/monitor/ZMonitorAutoConfiguration.java
+++ src/main/java/com/z/monitor/ZMonitorAutoConfiguration.java
@@ -14,8 +14,8 @@ public class ZMonitorAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public ZMonitorMetricsProvider zMonitorMetricsProvider() {
-        return new ZMonitorDefaultMetricsProvider();
+    public ZMonitorMetricsProvider zMonitorMetricsProvider(io.micrometer.core.instrument.MeterRegistry meterRegistry) {
+        return new ZMonitorDefaultMetricsProvider(meterRegistry);
     }
 
     @Bean
@@ -28,22 +28,22 @@ public class ZMonitorAutoConfiguration {
         return new ZMonitorMetricsController(providers);
     }
 
-            @Bean
-            public ZMonitorAuthController zMonitorAuthController(ZMonitorProperties properties) {
-                return new ZMonitorAuthController(properties);
-            }
-        
-            @org.springframework.context.annotation.Configuration
-            @org.springframework.boot.autoconfigure.condition.ConditionalOnClass(name = "org.springframework.security.web.SecurityFilterChain")
-            static class ZMonitorSecurityConfiguration {
-                @Bean
-                public org.springframework.boot.web.servlet.FilterRegistrationBean<ZMonitorSecurityFilter> zMonitorSecurityFilter() {
-                    org.springframework.boot.web.servlet.FilterRegistrationBean<ZMonitorSecurityFilter> registrationBean = new org.springframework.boot.web.servlet.FilterRegistrationBean<>();
-                    registrationBean.setFilter(new ZMonitorSecurityFilter());
-                    registrationBean.addUrlPatterns("/z-monitor/*", "/actuator/*");
-                    registrationBean.setOrder(org.springframework.core.Ordered.HIGHEST_PRECEDENCE);
-                    return registrationBean;
-                }
-            }
+    @Bean
+    public ZMonitorAuthController zMonitorAuthController(ZMonitorProperties properties) {
+        return new ZMonitorAuthController(properties);
+    }
+
+    @org.springframework.context.annotation.Configuration
+    @org.springframework.boot.autoconfigure.condition.ConditionalOnClass(name = "org.springframework.security.web.SecurityFilterChain")
+    static class ZMonitorSecurityConfiguration {
+        @Bean
+        public org.springframework.boot.web.servlet.FilterRegistrationBean<ZMonitorSecurityFilter> zMonitorSecurityFilter() {
+            org.springframework.boot.web.servlet.FilterRegistrationBean<ZMonitorSecurityFilter> registrationBean = new org.springframework.boot.web.servlet.FilterRegistrationBean<>();
+            registrationBean.setFilter(new ZMonitorSecurityFilter());
+            registrationBean.addUrlPatterns("/z-monitor/*", "/actuator/*");
+            registrationBean.setOrder(org.springframework.core.Ordered.HIGHEST_PRECEDENCE);
+            return registrationBean;
         }
+    }
+}
         
